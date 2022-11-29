99d3e1df0f0a4b3ab58f54040bcebb606ccaa321
Fernanda <sanjaya.dev20@gmail.com>
2022-11-30 01:58:40 +0700

    membuat tampilan pesan tiket

--- src/components/BayarTiket/index.js
+++ src/components/BayarTiket/index.js
@@ -9,53 +9,103 @@ const BayarTiket = () => {
     <div>
       <Header />
       <Container>
-        <div className="row" style={{paddingTop: "10px"}}>
+        <div className="row" style={{ paddingTop: "10px" }}>
           <div className="col-lg-12">
-          <nav class="navbar navbar-expand-lg" style={{backgroundColor: "#4E4E4E"}}>
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#" style={{color: "#FFFFFF"}}>Booking</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto ">
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#" style={{color: "#FFFFFF"}}>Pilih Tiket</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#" style={{color: "#FFFFFF"}}>Pesan Tiket</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#" style={{color: "#FFFFFF"}}>Bayar</a>
-                  </li>
-                </ul>
+            <nav
+              class="navbar navbar-expand-lg"
+              style={{ backgroundColor: "#4E4E4E" }}
+            >
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#" style={{ color: "#FFFFFF" }}>
+                  Booking
+                </a>
+                <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                  class="collapse navbar-collapse "
+                  id="navbarSupportedContent"
+                >
+                  <ul class="navbar-nav ms-auto ">
+                    <li class="nav-item">
+                      <a
+                        class="nav-link"
+                        aria-current="page"
+                        href="#"
+                        style={{ color: "#FFFFFF" }}
+                      >
+                        Pilih Tiket
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#" style={{ color: "#FFFFFF" }}>
+                        Pesan Tiket
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a
+                        class="nav-link active"
+                        href="#"
+                        style={{ color: "#FFFFFF" }}
+                      >
+                        Bayar
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
-          </nav>
-            <h2 style={{marginTop: "40px"}}>
-              <img src="img/park-tickets-couple 1.png" alt="tiket" style={{paddingRight: "15px"}}/>
+            </nav>
+            <h2 style={{ marginTop: "40px" }}>
+              <img
+                src="img/park-tickets-couple 1.png"
+                alt="tiket"
+                style={{ paddingRight: "15px" }}
+              />
               Metode Pengiriman
             </h2>
-            <section style={{marginLeft: "60px",paddingLeft: "15px"}}>
+            <section style={{ marginLeft: "60px", paddingLeft: "15px" }}>
               <h3>Tiket Elektronik</h3>
               <p>
                 Inilah cara hemat waktu dan tanpa kertas untuk bepergian. Anda
                 akan menerima referensi tiket elektronik melalui e-mail.
               </p>
             </section>
-            <h2 style={{paddingTop: "20px"}}>
-              <img src="/img/Vector.png" alt="" style={{paddingRight: "15px"}} />
+            <h2 style={{ paddingTop: "20px" }}>
+              <img
+                src="/img/Vector.png"
+                alt=""
+                style={{ paddingRight: "15px" }}
+              />
               Pembayaran
             </h2>
-            <div style={{textAlign: "center", width: "1050px",height: "70px", marginLeft: "70px",backgroundColor: "#9f9f9f",borderRadius: "8px"}}>
-              <p style={{paddingTop: "20px"}}>Jumlah yang harus di bayar adalah IDR 1,921,020</p>
+            <div
+              style={{
+                textAlign: "center",
+                width: "1050px",
+                height: "70px",
+                marginLeft: "70px",
+                backgroundColor: "#9f9f9f",
+                borderRadius: "8px",
+              }}
+            >
+              <p style={{ paddingTop: "20px" }}>
+                Jumlah yang harus di bayar adalah IDR 1,921,020
+              </p>
             </div>
-            <section style={{marginLeft: "60px",paddingLeft: "15px"}}>
+            <section style={{ marginLeft: "60px", paddingLeft: "15px" }}>
               <h3>Pembayaran Online</h3>
               <p>
                 Pembayaran Online Anda akan dialihkan ke halaman eksternal bila
                 mengklik tombol Konfirmasi perjalanan. Metode pembayaran yang
-                tersedia antara lain: 
+                tersedia antara lain:
               </p>
               <ul>
                 <li>
@@ -69,12 +119,23 @@ const BayarTiket = () => {
                 </li>
               </ul>
             </section>
-            <button className="btn_bayar" style={{borderRadius: "26px", width: "250px",height: "50px",marginLeft: "450px",color: "#ffff"}}>Bayar</button>
+            <button
+              className="btn_bayar"
+              style={{
+                borderRadius: "26px",
+                width: "250px",
+                height: "50px",
+                marginLeft: "450px",
+                color: "#ffff",
+              }}
+            >
+              Bayar
+            </button>
           </div>
         </div>
       </Container>
-      <br/>
-      <br/>
+      <br />
+      <br />
       <Footer />
     </div>
   );
--- src/components/PesanTiket/index.js
+++ src/components/PesanTiket/index.js
@@ -2,11 +2,107 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../Header";
 import Footer from "../Footer";
+import { Container, Navbar, NavbarBrand } from "react-bootstrap";
 
 const PesanTiket = () => {
   return (
     <div>
       <Header />
+      <Container>
+        <nav
+          class="navbar navbar-expand-lg"
+          style={{ backgroundColor: "#4E4E4E" }}
+        >
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#" style={{ color: "#FFFFFF" }}>
+              Booking
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto ">
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    aria-current="page"
+                    href="#"
+                    style={{ color: "#FFFFFF" }}
+                  >
+                    Pilih Tiket
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" style={{ color: "#FFFFFF" }}>
+                    Pesan Tiket
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link active"
+                    href="#"
+                    style={{ color: "#FFFFFF" }}
+                  >
+                    Bayar
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-lg-6">
+            <h3>Data Diri</h3>
+            <form>
+              <div className="col-lg-12">
+                <label>Nama Lengkap</label><br/>
+                <input type={"text"} style={{width: "200px"}}></input>
+              </div>
+              <div className="row">
+                <div className="col-lg-4">
+                  <label>Kursi</label><br/>
+                  <input type={"text"} style={{width: "140px"}}></input>
+                </div>
+                <div className="col-lg-4">
+                  <label>Makanan</label><br/>
+                  <input type={"text"} style={{width: "140px"}}></input>
+                </div>
+                <div className="col-lg-4">
+                  <label>Bagasi</label><br/>
+                  <input type={"text"} style={{width: "140px"}}></input>
+                </div>
+              </div>
+              <h3>Informasi Kontak</h3>
+              <div className="row">
+                <div className="col-lg-6">
+                  <label>Email</label><br/>
+                  <input type={"email"}></input><br/>
+                  <label>Re-Email</label><br/>
+                  <input type={"email"}></input>
+                </div>
+                <div className="col-lg-6">
+                  <label>Home Phone</label>
+                  <input type={"tel"}></input>
+                  <label>Mobile Phone</label>
+                  <input type={"tel"}></input>
+                </div>
+              </div>
+              
+            </form>
+          </div>
+          <div className="col-lg-6">
+
+          </div>
+        </div>
+      </Container>
       <Footer />
     </div>
   );
