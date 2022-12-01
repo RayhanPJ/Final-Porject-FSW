29fbf5bb4cb2914c5b596eee297cfd5163625950
Fernanda <sanjaya.dev20@gmail.com>
2022-12-01 11:06:32 +0700

    memperbaiki footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,13 +6,13 @@ function Footer() {
     <footer className="pt-3">
       <Container>
         <div className="row d-flex align-content-center">
-          <div className="col">
+          <div className="col-lg-3 col">
             <img src="/img/GoTravel.png" alt="GoTravel" />
             <br />
             <p className="text-light">Copyright Go Travel 2022</p>
           </div>
           {/* <!-- social media --> */}
-          <div className="col sosmed">
+          <div className="col-lg-3 col sosmed">
             <p>Connect With Us</p>
             <a href="tes">
               <img src="/img/icon_facebook.png" alt="" />
@@ -28,7 +28,7 @@ function Footer() {
             </a>
           </div>
           {/* <!-- navigation --> */}
-          <div className="col">
+          <div className="col-lg-3">
             <div className="nav_footer">
               <a href="tes">
                 <p>Beranda</p>
@@ -44,7 +44,7 @@ function Footer() {
               </a>
             </div>
           </div>
-          <div className="col addres">
+          <div className="col-lg-3 addres">
             <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo Indonesia</p>
             <p>gotravel@gmail.com</p>
             <p>081-233-334-808</p>
